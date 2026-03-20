b34dbd24d82ba86b19e65e4c8588ca3c3b49ada5
Brefkaa <brefkaa@gmatc.matc.edu>
2026-03-20 12:06:15 -0500

    added styled components and theme

---
+++ blog/src/components/themes/Gray/Gray.js
@@ -0,0 +1,8 @@
+const theme = {
+    header: {
+        background: "var(--color-primary)",
+        color: "var(--color-contrast)"
+    }
+}
+
+export { theme as Gray }
--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data }) => {
     const { title } = data.contentfulBlogPost;
@@ -8,6 +9,8 @@ const BlogPost = ({ data }) => {
     return (
         <Layout>
             <h1>{title}</h1>
+            <div><GatsbyImage image={data.contentfulBlogPost.heroImage.gatsbyImageData} /></div>
+            <div dangerouslySetInnerHTML={{ __html: data.contentfulBlogPost.body.childMarkdownRemark.html }} />
         </Layout>
     )
 }
@@ -16,9 +19,21 @@ export default BlogPost;
 
 export const pageQuery = graphql`
   query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: { eq: $slug }) {
-      title
-      slug
+        contentfulBlogPost(slug: { eq: $slug }) {
+            title
+            slug
+            body {
+                childMarkdownRemark {
+                    html
+                }
+            }
+            heroImage {
+                gatsbyImageData(
+                    layout: CONSTRAINED
+                    placeholder: BLURRED
+                    width: 300
+                )
+            }
+        }
     }
-  }
 `
